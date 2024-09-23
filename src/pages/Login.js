ab0d0b2c4fafe56d8e5adc899c88847b49bde72f
Daniel <dlezama0796@gmail.com>
2024-09-23 10:03:06 -0300

    link to orders

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -8,6 +8,9 @@ function Login() {
 			<button>
 				<Link to="/task/manager">task manager</Link>
 			</button>
+			<button>
+				<Link to="/orders/all">ordenes</Link>
+			</button>
 		</div>
 	);
 }
