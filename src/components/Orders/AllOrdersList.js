72609f360dbcc81cda7279a8e54b51f34a8d459f
Daniel <dlezama0796@gmail.com>
2025-02-27 09:58:29 -0300

    laptop updates

--- src/components/Orders/AllOrdersList.js
+++ src/components/Orders/AllOrdersList.js
@@ -1,111 +1,98 @@
 import React from 'react';
 import styles from './orders.module.css';
-import {
-	Table,
-	Tbody,
-	Td,
-	Th,
-	Thead,
-	Tr,
-} from '../shared/Tables';
-import {
-	isUrgent,
-	isWarning,
-} from '../../utilities/functions/dates';
+import { Table, Tbody, Td, Th, Thead, Tr } from '../shared/Tables';
+import { isUrgent, isWarning } from '../../utilities/functions/dates';
 import Loader from '../shared/Loader';
 import Dropdown from '../shared/Dropdown';
 import { today } from '../../utilities/functions/dates';
 import { IconButton } from '@mui/material';
+import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
 import EditIcon from '@mui/icons-material/Edit';
 import { useNavigate } from 'react-router-dom'; // Cambio aquí
 
-export function AllOrdersList({
-	orders,
-	ordersLoading,
-	changeStatus,
-}) {
-	const statusList = [
-		'Todos',
-		'Aceptada',
-		'Abierta',
-		'Finalizada',
-		'Detenida',
-		'Facturada',
-	];
+export function AllOrdersList({ orders, ordersLoading, changeStatus }) {
+  const statusList = [
+    'Todos',
+    'Aceptada',
+    'Abierta',
+    'Finalizada',
+    'Detenida',
+    'Facturada',
+  ];
 
-	const navigate = useNavigate(); // Cambio aquí
+  const navigate = useNavigate(); // Cambio aquí
 
-	const handleEditClick = (order) => {
-		navigate('/admin/orders/form', { state: order }); // Cambio aquí
-	};
+  const handleEditClick = (order) => {
+    navigate('/admin/orders/form', { state: order }); // Cambio aquí
+  };
 
-	return (
-		<div className={styles.allOrders}>
-			<div className={styles.allOrdersTable}>
-				{ordersLoading ? (
-					<div className={styles.loader}>
-						<Loader />
-					</div>
-				) : (
-					<div className={styles.tables}>
-						<Table>
-							<Thead>
-								<Tr>
-									<Th size={'small'}>Nº orden</Th>
-									<Th>Producto</Th>
-									<Th size={'big'}>Cliente</Th>
-									<Th>
-										<p>Estado</p>{' '}
-										<Dropdown
-											options={statusList}
-											handleSelect={changeStatus}
-											dark
-										/>
-									</Th>
-									<Th>Creado</Th>
-									<Th>Fecha límite</Th>
-									<Th>Presupuesto</Th>
-									<th className={styles.editTh}>Editar</th>
-								</Tr>
-							</Thead>
-							<Tbody>
-								{orders.map((order) => (
-									<Tr
-										key={order?._id}
-										warning={isWarning(order)}
-										urgent={isUrgent(order)}
-									>
-										<Td size={'small'}>
-											{order?.orderNumber}
-										</Td>
-										<Td>{order?.product}</Td>
-										<Td size={'big'}>
-											{order?.client?.companyName}
-										</Td>
-										<Td>{order?.status}</Td>
-										<Td>{order?.dateCreated}</Td>
-										<Td>{order?.dateFinal}</Td>
-										<Td>${order?.budget}</Td>
-										<td className={styles.editTd}>
-											<IconButton
-												style={{ padding: 0 }}
-												onClick={() =>
-													handleEditClick(order)
-												} // Cambio aquí
-											>
-												<EditIcon
-													fontSize="small"
-													sx={{ color: '#101204' }}
-												/>
-											</IconButton>
-										</td>
-									</Tr>
-								))}
-							</Tbody>
-						</Table>
-					</div>
-				)}
-			</div>
-		</div>
-	);
+  return (
+    <div className={styles.allOrders}>
+      <div className={styles.allOrdersTable}>
+        {ordersLoading ? (
+          <div className={styles.loader}>
+            <Loader />
+          </div>
+        ) : (
+          <div className={styles.tables}>
+            <Table>
+              <Thead>
+                <Tr>
+                  <Th size={'small'}>Nº orden</Th>
+                  <Th>Producto</Th>
+                  <Th size={'big'}>Cliente</Th>
+                  <Th>
+                    <p>Estado</p>{' '}
+                    <Dropdown
+                      options={statusList}
+                      handleSelect={changeStatus}
+                      dark
+                    />
+                  </Th>
+                  <Th>Creado</Th>
+                  <Th>Fecha límite</Th>
+                  <Th>Presupuesto</Th>
+
+                  <Th>Crear Orden</Th>
+                  <Th>Crear Presupuesto</Th>
+                  <th className={styles.editTh}>Editar</th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {orders.map((order) => (
+                  <Tr
+                    key={order?._id}
+                    warning={isWarning(order)}
+                    urgent={isUrgent(order)}
+                  >
+                    <Td size={'small'}>{order?.orderNumber}</Td>
+                    <Td>{order?.product}</Td>
+                    <Td size={'big'}>{order?.client?.companyName}</Td>
+                    <Td>{order?.status}</Td>
+                    <Td>{order?.dateCreated}</Td>
+                    <Td>{order?.dateFinal}</Td>
+                    <Td>${order?.budget}</Td>
+                    <Td>
+                      <PictureAsPdfIcon color='error' /* fontSize='large' */ />
+                    </Td>
+                    <Td>
+                      <PictureAsPdfIcon color='error' /* fontSize='large' */ />
+                    </Td>
+                    <td className={styles.editTd}>
+                      <IconButton
+                        style={{ padding: 0 }}
+                        onClick={() => handleEditClick(order)} // Cambio aquí
+                      >
+                        <EditIcon fontSize='small' sx={{ color: '#101204' }} />
+                      </IconButton>
+                    </td>
+                  </Tr>
+                ))}
+              </Tbody>
+            </Table>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
--- src/components/Resources/Operations/OperationsForm.js
+++ src/components/Resources/Operations/OperationsForm.js
@@ -1,242 +1,214 @@
 import styles from './operations.module.css';
 import Select from 'react-select';
-import {
-	Button,
-	Checkbox,
-	IconButton,
-} from '@mui/material';
+import { Button, Checkbox, IconButton } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 import { changeValue } from '../../../utilities/functions/forms/fields';
 import { useOperationsForm } from '../../../utilities/customHooks/forms/operationsForm';
 
 function OperationsForm({
-	isEdit,
-	selectStyles,
-	setOpenOperationModal,
-	fields,
-	setFields,
+  isEdit,
+  selectStyles,
+  setOpenOperationModal,
+  fields,
+  setFields,
 }) {
-	const {
-		stationsOptions,
-		progressiveCheck,
-		unitTypeOptions,
-		handleCheckBox,
-		handleRangeChange,
-		handleNewRange,
-		deleteRangeRow,
-		setSelect,
-		handleSubmit,
-	} = useOperationsForm(
-		fields,
-		setFields,
-		isEdit,
-		setOpenOperationModal
-	);
+  const {
+    stationsOptions,
+    progressiveCheck,
+    unitTypeOptions,
+    handleCheckBox,
+    handleRangeChange,
+    handleNewRange,
+    deleteRangeRow,
+    setSelect,
+    handleSubmit,
+  } = useOperationsForm(fields, setFields, isEdit, setOpenOperationModal);
 
-	return (
-		<div className={styles.formContainer}>
-			{isEdit ? (
-				<h2>Editar Operación</h2>
-			) : (
-				<h2>Nueva Operación</h2>
-			)}
-			<form>
-				<div className={styles.labelInput}>
-					<label>Nombre:</label>
-					<input
-						name="name"
-						value={fields.name ?? ''}
-						className={styles.input}
-						onChange={(e) => changeValue(e, setFields)}
-					/>
-				</div>
-				<div className={styles.labelInput}>
-					<label>Estación</label>
-					<div className={styles.input}>
-						<Select
-							name="workStation"
-							value={
-								stationsOptions.find(
-									(station) =>
-										station.value ===
-											fields.workStation?._id ||
-										station.value === fields.workStation
-								) ?? ''
-							}
-							styles={selectStyles}
-							options={stationsOptions}
-							onChange={(option, e) => setSelect(option, e)}
-						/>
-					</div>
-				</div>
-				{fields.isPrintable ? (
-					<div className={styles.labelInput}>
-						<label>Precio chapa:</label>
-						<input
-							name="plateCost"
-							value={fields.plateCost ?? ''}
-							className={styles.input}
-							onChange={(e) => changeValue(e, setFields)}
-						/>
-					</div>
-				) : (
-					''
-				)}
-				<div className={styles.labelInput}>
-					<label>Tipo de unidad</label>
-					<div className={styles.input}>
-						<Select
-							name="unitType"
-							value={
-								unitTypeOptions.find(
-									(unit) => unit.value === fields.unitType
-								) ?? ''
-							}
-							styles={selectStyles}
-							options={unitTypeOptions}
-							onChange={(option, e) => setSelect(option, e)}
-						/>
-					</div>
-				</div>
-				<div className={styles.labelInput}>
-					<label>Precio por unidad:</label>
-					<input
-						name="unitCost"
-						value={fields.unitCost ?? ''}
-						className={styles.input}
-						onChange={(e) => changeValue(e, setFields)}
-					/>
-				</div>
-				<div className={styles.labelInput}>
-					<label>Precio mínimo:</label>
-					<input
-						name="minPrice"
-						value={fields.minPrice ?? ''}
-						className={styles.input}
-						onChange={(e) => changeValue(e, setFields)}
-					/>
-				</div>
-				<div className={styles.labelInput}>
-					<label>Precio progresivo?</label>
-					<div className={styles.input}>
-						<Checkbox
-							checked={progressiveCheck}
-							name="progressivePrice"
-							onChange={handleCheckBox}
-						/>
-					</div>
-				</div>
-				{fields.progressivePrice ? (
-					<table>
-						<thead>
-							<tr>
-								<th>Desde</th>
-								<th>Hasta</th>
-								<th>Cantidad</th>
-								<th>Precio por cantidad</th>
-							</tr>
-						</thead>
-						<tbody>
-							{fields.pricingRules.map((range, index) => (
-								<tr key={index}>
-									<td>
-										<input
-											name="rangeStart"
-											className={styles.priceInput}
-											value={range?.rangeStart}
-											onChange={(e) =>
-												handleRangeChange(e, index)
-											}
-										/>
-									</td>
-									<td>
-										<input
-											name="rangeEnd"
-											className={styles.priceInput}
-											value={range?.rangeEnd}
-											onChange={(e) =>
-												handleRangeChange(e, index)
-											}
-										/>
-									</td>
-									<td>
-										<input
-											name="step"
-											className={styles.priceInput}
-											value={range?.step}
-											onChange={(e) =>
-												handleRangeChange(e, index)
-											}
-										/>
-									</td>
-									<td>
-										<input
-											name="price"
-											className={styles.priceInput}
-											value={range?.price}
-											onChange={(e) =>
-												handleRangeChange(e, index)
-											}
-										/>
-									</td>
-									<td>
-										<IconButton
-											color="error"
-											onClick={() => deleteRangeRow(index)}
-											sx={{
-												height: '20px',
-												width: '20px',
-											}}
-										>
-											<CloseIcon />
-										</IconButton>
-									</td>
-								</tr>
-							))}
-							<tr>
-								<td className={styles.tdButton}>
-									<Button
-										variant="contained"
-										sx={{ fontSize: '10px' }}
-										onClick={handleNewRange}
-									>
-										nuevo rango
-									</Button>
-								</td>
-							</tr>
-						</tbody>
-					</table>
-				) : (
-					''
-				)}
-				<div className={styles.labelInput}>
-					<label>Obligatorio?</label>
-					<div className={styles.input}>
-						<Checkbox
-							value={fields.isAllTask ?? false}
-							name="isAllTask"
-							onChange={handleCheckBox}
-						/>
-					</div>
-				</div>
-				{isEdit ? (
-					<Button
-						variant="contained"
-						onClick={handleSubmit}
-					>
-						Editar
-					</Button>
-				) : (
-					<Button
-						variant="contained"
-						onClick={handleSubmit}
-					>
-						Crear
-					</Button>
-				)}
-			</form>
-		</div>
-	);
+  return (
+    <div className={styles.formContainer}>
+      {isEdit ? <h2>Editar Operación</h2> : <h2>Nueva Operación</h2>}
+      <form>
+        <div className={styles.labelInput}>
+          <label>Nombre:</label>
+          <input
+            name='name'
+            value={fields.name ?? ''}
+            className={styles.input}
+            onChange={(e) => changeValue(e, setFields)}
+          />
+        </div>
+        <div className={styles.labelInput}>
+          <label>Estación</label>
+          <div className={styles.input}>
+            <Select
+              name='workStation'
+              value={
+                stationsOptions.find(
+                  (station) =>
+                    station.value === fields.workStation?._id ||
+                    station.value === fields.workStation
+                ) ?? ''
+              }
+              styles={selectStyles}
+              options={stationsOptions}
+              onChange={(option, e) => setSelect(option, e)}
+            />
+          </div>
+        </div>
+        {fields.isPrintable ? (
+          <div className={styles.labelInput}>
+            <label>Precio chapa:</label>
+            <input
+              name='plateCost'
+              value={fields.plateCost ?? ''}
+              className={styles.input}
+              onChange={(e) => changeValue(e, setFields)}
+            />
+          </div>
+        ) : (
+          ''
+        )}
+        <div className={styles.labelInput}>
+          <label>Tipo de unidad</label>
+          <div className={styles.input}>
+            <Select
+              name='unitType'
+              value={
+                unitTypeOptions.find(
+                  (unit) => unit.value === fields.unitType
+                ) ?? ''
+              }
+              styles={selectStyles}
+              options={unitTypeOptions}
+              onChange={(option, e) => setSelect(option, e)}
+            />
+          </div>
+        </div>
+        <div className={styles.labelInput}>
+          <label>Precio por unidad:</label>
+          <input
+            name='unitCost'
+            value={fields.unitCost ?? ''}
+            className={styles.input}
+            onChange={(e) => changeValue(e, setFields)}
+          />
+        </div>
+        <div className={styles.labelInput}>
+          <label>Precio mínimo:</label>
+          <input
+            name='minPrice'
+            value={fields.minPrice ?? ''}
+            className={styles.input}
+            onChange={(e) => changeValue(e, setFields)}
+          />
+        </div>
+        <div className={styles.labelInput}>
+          <label>Precio por rangos?</label>
+          <div className={styles.input}>
+            <Checkbox
+              checked={progressiveCheck}
+              name='progressivePrice'
+              onChange={handleCheckBox}
+            />
+          </div>
+        </div>
+        {fields.progressivePrice ? (
+          <table>
+            <thead>
+              <tr>
+                <th>Desde</th>
+                <th>Hasta</th>
+                <th>Cantidad</th>
+                <th>Precio por cantidad</th>
+              </tr>
+            </thead>
+            <tbody>
+              {fields.pricingRules.map((range, index) => (
+                <tr key={index}>
+                  <td>
+                    <input
+                      name='rangeStart'
+                      className={styles.priceInput}
+                      value={range?.rangeStart}
+                      onChange={(e) => handleRangeChange(e, index)}
+                    />
+                  </td>
+                  <td>
+                    <input
+                      name='rangeEnd'
+                      className={styles.priceInput}
+                      value={range?.rangeEnd}
+                      onChange={(e) => handleRangeChange(e, index)}
+                    />
+                  </td>
+                  <td>
+                    <input
+                      name='step'
+                      className={styles.priceInput}
+                      value={range?.step}
+                      onChange={(e) => handleRangeChange(e, index)}
+                    />
+                  </td>
+                  <td>
+                    <input
+                      name='price'
+                      className={styles.priceInput}
+                      value={range?.price}
+                      onChange={(e) => handleRangeChange(e, index)}
+                    />
+                  </td>
+                  <td>
+                    <IconButton
+                      color='error'
+                      onClick={() => deleteRangeRow(index)}
+                      sx={{
+                        height: '20px',
+                        width: '20px',
+                      }}
+                    >
+                      <CloseIcon />
+                    </IconButton>
+                  </td>
+                </tr>
+              ))}
+              <tr>
+                <td className={styles.tdButton}>
+                  <Button
+                    variant='contained'
+                    sx={{ fontSize: '10px' }}
+                    onClick={handleNewRange}
+                  >
+                    nuevo rango
+                  </Button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        ) : (
+          ''
+        )}
+        <div className={styles.labelInput}>
+          <label>Obligatorio?</label>
+          <div className={styles.input}>
+            <Checkbox
+              value={fields.isAllTask ?? false}
+              name='isAllTask'
+              onChange={handleCheckBox}
+            />
+          </div>
+        </div>
+        {isEdit ? (
+          <Button variant='contained' onClick={handleSubmit}>
+            Editar
+          </Button>
+        ) : (
+          <Button variant='contained' onClick={handleSubmit}>
+            Crear
+          </Button>
+        )}
+      </form>
+    </div>
+  );
 }
 
 export default OperationsForm;
